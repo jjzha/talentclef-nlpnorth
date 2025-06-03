05aee78f507676591b4edfda7d995a0e7337ace7
Mike Zhang <jjz@cs.aau.dk>
2025-06-03 11:02:17 +0200

    add running scripts

---
+++ scripts/1.preprocess_taskb_slurm.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#SBATCH --gres=gpu:a10
+#SBATCH --job-name=esco-data-process
+#SBATCH --output=logs/preprocess_data_%j.out
+#SBATCH --nodes=1 
+#SBATCH --ntasks=1 
+#SBATCH --cpus-per-task=1 
+#SBATCH --time=12:00:00 
+#SBATCH --mem=128G
+
+
+source $HOME/.bashrc 
+singularity exec --nv ~/conda_container.sif python3 scripts/preprocess_skill_nce_loss_description.py
---
+++ scripts/3.evaluate_taska.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+#SBATCH --gres=gpu:l40s
+#SBATCH --job-name=eval_talentclef
+#SBATCH --output=logs/eval_talentclef_%j.out
+#SBATCH --error=logs/eval_talentclef_%j.err
+#SBATCH --nodes=1 
+#SBATCH --ntasks=1 
+#SBATCH --cpus-per-task=1 
+#SBATCH --time=2:00:00 
+#SBATCH --mem=128G
+
+export HF_TOKEN="" # for Hugging Face access token, if needed
+source $HOME/.bashrc 
+
+singularity exec --nv ~/conda_container.sif python3 scripts/evaluate_taska.py \
+  --validation_root "" \
+  --model_path "" \
+  --output_dir "" \
+  --languages "" \
+  --min_top_k 1 \
+  --max_top_k 5000 \
+  --threshold_start 0.05 \
+  --threshold_end 0.10 \
+  --threshold_step 0.05 \
+  --custom_tag "" \
+  # --instruct_prompt "Given a job title, retrieve the most similar job titles" \ # if you work with instruct models, uncomment this line
+  # --e5_prefix \ # if you are working with E5 models, uncomment this line
+  
