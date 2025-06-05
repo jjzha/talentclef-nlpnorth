93a68d235df9733db2d64f984689898c06bf3cb5
Rob <robvanderg@live.nl>
2025-06-05 16:08:36 +0200

    Updated runall.sh and README

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # talentclef-nlpnorth
+
+This repository contains the code to reproduce the results of the NLPnorth team at the TalenCLEF shared task (https://talentclef.github.io/talentclef/).
+
+The repository consists of a single folder that contains the scripts we used to obtain our predictions. The steps that we used (i.e. exact commands) can be found in `scripts/runall.sh`. We do not recommend to run this script, as it will take very long. Instead, use the commands there as a guide for reproducing certain results, and parallelize some of the steps. The scripts have a number as a prefix, which indicate their function:
+
+- 0: preparation of data and setup
+- 1: running the classification models
+- 2: running the contrastive models
+- 3: running the prompt-based models
+- 4: evaluate the models
+- 5: obtain the results overviews
+
+
+
--- scripts/runAll.sh
+++ scripts/runAll.sh
@@ -1,9 +1,21 @@
 # preprocessing
+./scripts/0.download.sh
+python3 scripts/0.prepTaskA.py
+python3 scripts/0.prepTaskB.py
 singularity exec --nv ~/conda_container.sif python3 scripts/0.preprocess_job_title_nce_loss_description.py
 singularity exec --nv ~/conda_container.sif python3 scripts/0.preprocess_skill_nce_loss_description.py
 
 # Training
 
+python3 scripts/1.classification.trainA.py > machamp/1.train.sh
+python3 scripts/1.classification.trainB.py >> machamp/1.train.sh
+cd machamp
+chmod +x 1.train.sh
+./1.train.sh
+# Or use slurm:
+# python3 scripts/misc/slurm.py 1.train.sh 1.train 4 24
+cd ../
+
 singularity exec --nv ~/conda_container.sif python3 scripts/2.contrastive.train.py \
   --data_path "" \
   --model_name "" \
@@ -21,7 +33,9 @@ singularity exec --nv ~/conda_container.sif python3 scripts/2.contrastive.train.
   --learning_rate 2e-6 \
   # --e5_prefix \ # if you are working with E5 models, uncomment this line
 
-
+# Evaluate:
+python3 scripts/4.evalA.py
+python3 scripts/4.evalB.py
 singularity exec --nv ~/conda_container.sif python3 scripts/4.evaluate_taska.py \
   --validation_root "" \
   --model_path "" \
